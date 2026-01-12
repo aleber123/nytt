92c847e0c165b8c909ce80e1067d6f7e8236e8ff
aleber123 <alexander.bergqvist@gmail.com>
2026-01-12 08:27:16 +0100

    Update favicon to DOX Box logo (same as doxvl.dk)
    - Replace custom SVG favicon with DOX Box logo from Danish office
    - Update PNG favicons (16x16, 32x32, apple-touch-icon) with DOX Box logo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,8 +48,7 @@ class MyDocument extends Document {
             />
           )}
           
-          {/* Favicon - SVG for modern browsers, PNG fallback */}
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          {/* Favicon - DOX Box logo */}
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
