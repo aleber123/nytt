30d85db3abcf471b65904833e7d4f23c07548d5e
aleber123 <alexander.bergqvist@gmail.com>
2026-01-12 08:14:49 +0100

    Fix favicon - use SVG with PNG fallbacks
    - Update _document.tsx to use SVG as primary favicon
    - Add PNG fallbacks (16x16, 32x32) for older browsers
    - Add apple-touch-icon.png (180x180) for iOS devices
    - Previous favicon.ico was incorrectly a WebP file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,9 +48,11 @@ class MyDocument extends Document {
             />
           )}
           
-          {/* Favicon */}
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+          {/* Favicon - SVG for modern browsers, PNG fallback */}
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         </Head>
         <body>
           <Main />
