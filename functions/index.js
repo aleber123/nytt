7b1765464503a574f9523eba699ac9ecaf6a1b27
aleber123 <alexander.bergqvist@gmail.com>
2026-01-19 14:41:38 +0100

    Add dotenv to load .env file for email configuration

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 // Initialize Firebase Admin
 admin.initializeApp();
